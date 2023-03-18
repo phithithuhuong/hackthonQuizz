6c649bce15e6ae79ceb9c1daf51e5fd3d8c4e8ae
phithithuhuong <thu872315@gmail.com>
2023-03-18 17:32:34 +0700

    template

---
+++ frontend/quizz/src/components/info/register.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+    MDBContainer,
+    MDBInput,
+    MDBCheckbox,
+    MDBBtn,
+    MDBIcon
+}
+    from 'mdb-react-ui-kit';
+
+function Register() {
+    return (
+        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+            <label style={{textAlign:"left"}}>Nhập tên </label>
+            <MDBInput wrapperClass='mb-4' id='form1' type='email'/>
+            <label style={{textAlign:"left"}}>Nhập mật khẩu</label>
+            <MDBInput wrapperClass='mb-4' id='form2' type='password'/>
+
+            <div className="d-flex justify-content-between mx-3 mb-4">
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Nhớ thông tin' />
+                <a href="!#">Quên mật khẩu ?</a>
+            </div>
+
+            <MDBBtn style={{background:"blueviolet", height:40}} className="mb-4">Đăng kí</MDBBtn>
+
+            <div className="text-center">
+                <p> <a href="!#">Quay lại đăng nhâp</a> </p>
+                <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
+                    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                        <MDBIcon fab icon='facebook-f' size="sm"/>
+                    </MDBBtn>
+
+                    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                        <MDBIcon fab icon='twitter' size="sm"/>
+                    </MDBBtn>
+
+                    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                        <MDBIcon fab icon='google' size="sm"/>
+                    </MDBBtn>
+
+                    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                        <MDBIcon fab icon='github' size="sm"/>
+                    </MDBBtn>
+
+                </div>
+            </div>
+
+        </MDBContainer>
+    );
+}
+
+export default Register;
