bdda6b1bc68bbb7867850bcdfb27f386686022a6
hungnkb <hungnkb@gmail.com>
2023-03-19 08:54:12 +0700

    pull fe

--- frontend/quizz/src/App.js
+++ frontend/quizz/src/App.js
@@ -2,10 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ShowAll from "./components/show/show-all";
+import { Route, Routes } from "react-router-dom";
+import Login from './components/info/login';
+import Register from './components/info/register';
 function App() {
   return (
     <div className="App">
-     <ShowAll/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+      </Routes>
+      <ShowAll />
     </div>
   );
 }
